0c575f9de92031948a6f6db33cc61119b3a12419
Gayatri132005 <gayatrikotwal13@gmail.com>
2025-05-07 22:03:10 +0530

    initial

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 const User = require("../model/User");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 
